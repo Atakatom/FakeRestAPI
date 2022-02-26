f66407cda0bdb512b0b5b218a4ce391bf448a561
Atakan Yontar <atakanyontar@gmail.com>
2022-02-26 12:42:16 +0300

    Patch method is written but not complete yet

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : true }))
 
+
+//get all users
 app.get("/users", (req, res) => {
     console.log("Request is received...");
     res.send(200, {
         db
     })
 })
+
+//get a user with its id
 app.get("/users/:id", (req, res) => {
     if(isNaN(req.params.id)){
         res.send(400, {
@@ -27,8 +31,43 @@ app.get("/users/:id", (req, res) => {
         }
     }
 })
-app.post("/users", (req, res) => {})
-app.patch("/users", (req, res) => {})
+
+// add a new user
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: new Date().getTime(),
+        full_name: req.body.full_name,
+        country: req.body.country,
+        email: req.body.email,
+        created_at: new Date()
+    };
+    db.push(newUser)
+    res.send(newUser)
+})
+
+// change some values of user
+app.patch("/users", (req, res) => {
+    if(isNaN(req.params.id)){
+        res.send(400, {
+            message: "Unprocessible Entry"
+        })
+    } else {
+        const user = db.find(u => u.id == req.params.id)
+        if(user){
+            if(userKeys.length === reqBodyKeys.length){
+                Object
+            }
+            Object.keys(req.body).forEach(key => {
+                user[key] = req.body[key];
+            })
+            res.send(200, user)
+        } else {
+            res.send(404, {
+                message: "User Not Found"
+            })
+        }
+    }
+})
 app.delete("/users/:id", (req, res) => {})
 
 app.listen(process.env.PORT || 3000, () => {
